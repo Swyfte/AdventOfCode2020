63df8353c57b7d28e8dbdc7e1cc81b7ba2bee098
Swyfte <arianwenpaganuzzi@gmail.com>
2020-12-02 00:04:00 +0000

    Update 1stDec.py

--- 1stDec.py
+++ 1stDec.py
@@ -1,5 +1,13 @@
 import os
-linelist[] = []
+linelist = []
 with open("input.txt") as f:
     for line in f:
         linelist.append(int(line))
+
+for x in linelist:
+    for y in linelist:
+        if x != y:
+            for z in linelist:
+                if (z != x) and (z != y):
+                    if (x + y + z == 2020):
+                        print (x * y * z)
