451635c78b3586f8970559d367caff27e9616199
Swyfte <arianwenpaganuzzi@gmail.com>
2020-12-02 18:56:57 +0000

    Update 02Dec.py

--- 02Dec.py
+++ 02Dec.py
@@ -3,7 +3,13 @@
 "Apply RegEx to second half"
 "If valid, incriment counter."
 
+import re
+
+miniumum = 0
+wholePattern = re.compile('^[0-9]+-[0-9]+\\s[a-zA-Z]:\\s[a-zA-Z]+$')
 linelist = []
 with open("input.txt") as f:
     for line in f:
-        linelist.append(int(line))
+        match = re.search('^[0-9]+', line)
+        miniumum = int(match.expand())
+        
