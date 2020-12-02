3bd1bcb5a17dfed6adb6d55caf504a9b7c15b9be
Swyfte <arianwenpaganuzzi@gmail.com>
2020-12-02 15:06:07 +0000

    Update 2ndDec.py

--- 2ndDec.py
+++ 2ndDec.py
@@ -4,3 +4,8 @@
 "Create RegEx from first half"
 "Apply RegEx to second half"
 "If valid, incriment counter."
+
+linelist = []
+with open("input.txt") as f:
+    for line in f:
+        linelist.append(int(line))
